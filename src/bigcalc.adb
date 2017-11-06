46e29d759d2f5c153feb7b51a1a568038e43e8d0
redtoorange <redtoorange@gmail.com>
2017-11-06 10:05:55 -0500

    Updated some docs

--- src/bigcalc.adb
+++ src/bigcalc.adb
@@ -1,6 +1,16 @@
-with Ada.Text_IO; use Ada.Text_IO;
+-- Andrew McGuiness
+-- ITEC 324 - Project Four
+-- November 6th, 2017
+--
+--
+--
+--
+
+with Ada.Text_IO; 
+use  Ada.Text_IO;
 
 procedure Bigcalc is
+
 
 begin
    Put ("Hello World!");
