7676c96415f9dd262367d817bcb539812bcf4765
redtoorange <redtoorange@gmail.com>
2017-11-15 18:45:59 -0500

    Added IO to the main class

--- src/bigcalc.adb
+++ src/bigcalc.adb
@@ -1,64 +1,98 @@
-with StackPkg;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
+with StackPkg;
+with BigNumPkg; use BigNumPkg;
+
+
 procedure bigcalc is
-   package IntStack is new StackPkg (10, Integer);
-   use IntStack;
+   type Input_Type is (NUMBER, OPERATOR, SPACE, OTHER);
 
-   myStack : IntStack.Stack;
-   temp    : Integer;
+   package BigNumStack is new StackPkg(100, BigNum);
+   use BigNumStack;
 
+   function Is_Operator(c : in Character) return Boolean
+   is
+   begin
+      if c = '+' or else c = '-' or else c ='*' then
+         return True;
+      else
+         return False;
+      end if;
+
+   end Is_Operator;
+
+   function Is_Number(c : in Character) return Boolean
+   is
+   begin
+      return Character'Pos(c) >= Character'Pos('0')
+        and then
+          Character'Pos(c) <= Character'Pos('9');
+   end Is_Number;
+
+
+   function Parse_Char(c : in Character) return Input_Type
+   is
+      result : Input_Type := OTHER;
+   begin
+      if Is_Number( c ) then
+         result := NUMBER;
+      elsif Is_Operator( c ) then
+         result := OPERATOR;
+      elsif c = ' ' then
+         result := SPACE;
+      end if;
+
+      return result;
+   end Parse_Char;
+
+   --
+   --     num      : BigNum;
+   --     numStack : BigNumStack.Stack;
 begin
-   Put_Line ("Pushing 10");
-   push (10, myStack);
-
-   Put_Line ("Pushing 11");
-   push (11, myStack);
-
-   Put ("Top: ");
-   temp := top (myStack);
-   Put_Line (temp'Image);
-
-   Put_Line ("Popping...");
-   pop (myStack);
-
-   Put ("Top: ");
-   temp := top (myStack);
-   Put_Line (temp'Image);
-   New_Line;
-
-   Put_Line ("Popping...");
-   pop (myStack);
-
+   declare
+      --line : String := Get_Line;
+      ch : Character;
+      nl : Boolean := False;
+      bn : BigNum;
+      op : Character;
+      trash : Character;
    begin
-      Put ("Top: ");
-      temp := top (myStack);
-      Put_Line (temp'Image);
-      New_Line;
-   exception
-      when Stack_Empty =>
-         Put_Line ("Caught Stack Empty correctly...");
+      Main_Loop:
+      loop
+         Look_Ahead(Item        => ch,
+                    End_Of_Line => nl);
+
+         if not nl then
+            if ch = 'q' then
+               Put_Line("Quitting");
+               exit Main_Loop;
+            elsif Is_Operator(ch) then
+               Get(op);
+               Put("Found an Operator <"); Put(op); Put_Line(">");
+            elsif Is_Number(ch) then
+               Get(bn);
+               Put("Found a Number <"); Put(bn); Put_Line(">");
+            else
+               Get(trash);
+               Put("Found something else <"); Put(trash); Put_Line(">");
+            end if;
+         elsif nl then
+            Skip_Line;
+         end if;
+      end loop Main_Loop;
+
+
    end;
 
-   begin
-      Put_Line ("Popping...");
-      pop (myStack);
-   exception
-      when Stack_Empty =>
-         Put_Line ("Caught Stack Empty correctly...");
-   end;
-
-   begin
-      for i in 1 .. 11 loop
-         Put_Line ("Pushing: " & i'Image);
-         push (i, myStack);
-         Put_Line ("Pushed: " & i'Image);
-      end loop;
-   exception
-      when Stack_Full =>
-         Put_Line ("Caught Stack Full correctly...");
-
-   end;
+   --     Put("Enter a BigNum: ");
+   --     Get( num );
+   --
+   --     New_Line;
+   --     Put("Pushing <"); Put(num); Put_Line("> on to stack.");
+   --     BigNumStack.push( num, numStack);
+   --     New_Line;
+   --     Put("Top of stack: ");
+   --     Put(top( numStack) );
 
 end bigcalc;
